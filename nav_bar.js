d49d2c290f190a7318a0e47dae66f42f6640d98b
yunagyuting77 <huangyuting77@gmail.com>
2026-05-05 16:43:23 +0800

    Add files via upload

---
+++ nav_bar.js
@@ -0,0 +1,17 @@
+  window.onload = function(){
+           document.getElementById("nav_bar").innerHTML = ` <nav>
+    <div class="container"><a href="index.html" class="logo">跑跑特訓營</a>
+<ul>
+    <li><a href="index.html">．回到首頁</a></li>
+    <li><a href="about.html" >．關於我們</a></li>
+    <li><a href="courses.html">．課程簡介</a></li>
+    <li><a href="tools.html">．配速工具</a></li>
+    <li><a href="testimonials.html">．學員見證</a></li>
+
+
+</ul>
+        </div>
+    </nav>`;
+
+
+       }
